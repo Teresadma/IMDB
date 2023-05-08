2d562afa228589d88137a630073906681944ed78
Teresa de Miguel <teresa.demiguel.alberti@gmail.com>
2023-05-08 15:42:29 +0200

    Subida 1

---
+++ professionalTest.ts
@@ -0,0 +1,12 @@
+import {Professional} from "./professional";
+
+let professional1: Professional = new Professional ("Pepito",45,70,185,true,"Española",4,"cantante");
+
+let professional2: Professional = new Professional ("Nanito",26,85,192,false,"Española",4,"peluquero");
+
+let professional3: Professional = new Professional ("Mayte",32,60,1170,true,"Española",4,"bailarina");
+
+professional1.printAll();
+professional2.printAll();
+professional3.printAll();
+
